3dfdd596c088da53e3a84b6d3fdf8cc2a429f8c8
thai-ru <robinthairu34@gmail.com>
2024-04-14 03:54:58 +0300

    initial commit

--- Pick-a-Pal/ContentView.swift
+++ Pick-a-Pal/ContentView.swift
@@ -8,12 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var names: [String] = ["John", "Erin", "Andrea", "Carter", "Jasmine"]
+    @State private var nameToAdd: String = ""
+    @State private var pickedName = ""
+    @State private var shouldRemovepickedName = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text(pickedName.isEmpty ? "" : pickedName)
+            
+            List {
+                ForEach(names, id: \.self) { name in
+                    Text(name)
+                }
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            
+            TextField("Add Name", text: $nameToAdd)
+                .autocorrectionDisabled()
+                .onSubmit {
+                    if !nameToAdd.isEmpty {
+                        names.append(nameToAdd)
+                        nameToAdd = ""
+                    }
+                }
+            
+            Toggle("Remove Picked name", isOn: $shouldRemovepickedName)
+            
+            Button{
+                if let randomName = names.randomElement() {
+                    pickedName = randomName
+                    
+                    if shouldRemovepickedName {
+                        names.removeAll {
+                            name in return (name == randomName)
+                        }
+                    }
+                } else {
+                    pickedName = ""
+                }
+            }label: {
+                Text("Pick random name")
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 16)
+            }
+        
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .padding(.top, 16)
         }
         .padding()
     }
